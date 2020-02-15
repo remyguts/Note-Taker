fae2d8b009c88804174b6fb5b5c1bc30e8297a5c
Remy Guts <remyguts@gmail.com>
2020-02-14 16:33:36 -0800

    connected the routing on server side

--- serverside.js
+++ serverside.js
@@ -5,6 +5,6 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-require('./routes/apiRoutes')(app);
-require('./routes/htmlRoutes')(app);
-app.listen(PORT, ())
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+app.listen(PORT, () => console.log("App listening on PORT: " + PORT));
