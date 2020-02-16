049efcfe24a07905cfd0c0761336f5c4f7c990c0
Remy Guts <remyguts@gmail.com>
2020-02-15 18:21:20 -0800

    created routes to styles and index.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,7 +1,16 @@
 "use strict";
 
 const path = require("path");
+
 module.exports = app => {
+  app.get("/assets/css/styles.css", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/assets/css/styles.css"));
+  });
+
+  app.get("/assets/js/index.js", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/assets/js/index.js"));
+  });
+
   app.get("/notes", (req, res) =>
     res.sendFile(path.join(__dirname, "../public/notes.html"))
   );
